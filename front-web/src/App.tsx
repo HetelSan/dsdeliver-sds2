d5ad95f173d249c761a78cb52b483faa6bfffd80
Hetel Santos <hetel.santos@gmail.com>
2021-01-07 09:28:42 -0300

    Adding Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css';
+import Home from './Home';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Navbar />
+      <Home />
     </div>
   );
 }
